392940a70af74c514599f5b770937179375f4a4a
Roman Bogdanov <bogdanovroman1221@gmail.com>
2017-04-11 02:22:30 +0300

    ready to check

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        /**
+         * Verification of weight and height
+         * by regular expressions
+         */
+        System.out.println("Enter your weight, kg:");
+        String weightString = sc.nextLine();
+        Pattern patternWeight = Pattern.compile("0*[4-9]\\d|0*1\\d\\d|0*200");
+        Matcher matcherWeight = patternWeight.matcher(weightString);
+
+        while (! matcherWeight.matches()){
+            System.out.println("Usage: 40 - 200");
+            weightString = sc.nextLine();
+            matcherWeight = patternWeight.matcher(weightString);
+        }
+
+        System.out.println("Enter your height, cm:");
+        String heightString = sc.nextLine();
+        Pattern patternHeight = Pattern.compile("0*[12]\\d\\d|300");
+        Matcher matcherHeight = patternHeight.matcher(heightString);
+
+        while (! matcherHeight.matches()){
+            System.out.println("Usage: 100 - 300");
+            heightString = sc.nextLine();
+            matcherHeight = patternWeight.matcher(heightString);
+        }
+
+        /**
+         * Calculating BMI and
+         * printing a message
+         */
+        int bmi = (int) Math.round(Double.parseDouble(weightString) * 10000 /
+                (Double.parseDouble(heightString) * Double.parseDouble(heightString)));
+
+//        System.out.println(Math.round(Double.parseDouble(weightString) * 10000 /
+//                (Double.parseDouble(heightString) * Double.parseDouble(heightString))));
+//        System.out.println(bmi);
+
+        switch (bmi){
+
+            case 13: case 14: case 15 :
+                System.out.println("BMI: " + bmi + " - Severe thinness");
+                break;
+
+            case 16: case 17: case 18 :
+                System.out.println("BMI: " + bmi + " - Underweight");
+                break;
+
+            case 19: case 20: case 21: case 22: case 23: case 24: case 25:
+                System.out.println("BMI: " + bmi + " - Normal (healthy weight)");
+                break;
+
+            case 26: case 27: case 28: case 29: case 30:
+                System.out.println("BMI: " + bmi + " - Overweight");
+                break;
+
+            case 31: case 32: case 33: case 34: case 35:
+                System.out.println("BMI: " + bmi + " - Obese Class I (Moderately obese)");
+                break;
+
+            case 36: case 37: case 38: case 39: case 40:
+                System.out.println("BMI: " + bmi + " - Obese Class II (Severely obese)");
+                break;
+
+            default:
+                System.out.println("BMI: " + bmi + " - To hospital!");
+                break;
+        }
+    }
+}
